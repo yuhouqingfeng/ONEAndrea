118bbaf1c92d6b6e577242479eb61b6ad4b255f7
wuxinyi <15091861910@163.com>
2018-05-13 18:32:08 +0800

    fouth

---
+++ Model/ONEDataBase.h
@@ -0,0 +1,13 @@
+//
+//  ONEDataBase.h
+//  ONEAndrea
+//
+//  Created by wuxinyi on 18/4/12.
+//  Copyright © 2018年 wuxinyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ONEDataBase : NSObject
+
+@end
---
+++ Model/ONEDataBase.m
@@ -0,0 +1,13 @@
+//
+//  ONEDataBase.m
+//  ONEAndrea
+//
+//  Created by wuxinyi on 18/4/12.
+//  Copyright © 2018年 wuxinyi. All rights reserved.
+//
+
+#import "ONEDataBase.h"
+
+@implementation ONEDataBase
+
+@end
